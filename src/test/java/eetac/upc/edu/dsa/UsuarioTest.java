668142f1b70304bc81263115e404936235da5ef7
munozloisivan <munozloisivan@gmail.com>
2017-03-06 19:16:09 +0100

    test Usuario

---
+++ src/test/java/eetac/upc/edu/dsa/UsuarioTest.java
@@ -0,0 +1,47 @@
+package eetac.upc.edu.dsa;
+
+import org.junit.Test;
+import java.util.Hashtable;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ivanm on 06/03/2017.
+ */
+public class UsuarioTest {
+
+    Controlador testControl = new Controlador();
+
+    @Test
+    public void testAddUserOK(){
+        Usuario us = new Usuario("ivan","pass","ivan@mail.com",1);
+        testControl.añadirUsuario(us);
+        assertTrue(testControl.mostrarListaUsuarios().hasMoreElements());
+    }
+
+    @Test
+    public void testDelUsuario(){
+        Usuario us = new Usuario("ivan","pass","ivan@mail.com",1);
+        testControl.añadirUsuario(us);
+        testControl.delUsuario(us.getId());
+        assertFalse(testControl.mostrarListaUsuarios().hasMoreElements());
+    }
+
+    @Test
+    public void testSearchUserByName(){
+        Usuario us = new Usuario("ivan","pass","ivan@mail.com",1);
+        testControl.añadirUsuario(us);
+        assertEquals(Usuario.class, testControl.searchUserByName("ivan").getClass());
+    }
+
+    @Test
+    public void testSearchUserApproach(){
+        Usuario us = new Usuario("ivan","pass","ivan@mail.com",1);
+        testControl.añadirUsuario(us);
+        Usuario us2 = new Usuario("ivana","passa","ivana@mail.com",1);
+        testControl.añadirUsuario(us2);
+        assertEquals(2,testControl.searchUserApproach("an").size());
+    }
+
+
+}
